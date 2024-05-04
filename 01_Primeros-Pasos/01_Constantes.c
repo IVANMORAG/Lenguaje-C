02adb8a189aea9bcf72b563a0d0cad8a77d8da76
IVANMORAG <ivanmoragarcia412@gmail.com>
2024-05-04 13:28:25 -0600

    Agregando archivos iniciales del proyecto

---
+++ 01_Primeros-Pasos/01_Constantes.c
@@ -0,0 +1,15 @@
+// Incluir la biblioteca estándar necesaria para entrada/salida.
+#include <stdio.h>
+
+// Definir una constante llamada PI con el valor 3.1416.
+#define PI 3.1416
+
+// Método principal del programa.
+int main() {
+    
+    // Imprimir el valor de la constante PI en formato de punto flotante.
+    printf("PI vale %f \n", PI);
+
+    // Indicar que el programa finalizó correctamente devolviendo 0.
+    return 0;
+}
